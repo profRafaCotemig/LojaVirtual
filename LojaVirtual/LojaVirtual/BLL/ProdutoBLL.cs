4120df6011f10dc074e4e39078d43500301627b9
profRafaCotemig <rafaelapriscila@cotemig.com>
2021-04-09 11:06:02 -0300

    Update ProdutoBLL.cs

--- LojaVirtual/LojaVirtual/BLL/ProdutoBLL.cs
+++ LojaVirtual/LojaVirtual/BLL/ProdutoBLL.cs
@@ -26,6 +26,34 @@ namespace LojaVirtual.BLL
                                                 '{produto.QuantidadeEstoque}');");
             con.ExecutarSQL(sql);
         }
+        public void Excluir(ProdutoDTO produto) {
+            string sql = string.Format($@"DELETE FROM produto WHERE id= {produto.Id};");
+            con.ExecutarSQL(sql);
+        }
+        public void Alterar(ProdutoDTO produto) {
+            string sql = string.Format($@"UPDATE produto SET nome= '{produto.Nome}',
+                                                             descricao= '{produto.Descricao}',
+                                                             valor='{produto.Valor}',
+                                                             fornecedorID='{produto.FornecedorID}',
+                                                             categoriaID='{produto.CategoriaID}',
+                                                             foto = '{produto.Foto}',
+                                                             quantidadeEstoque='{produto.QuantidadeEstoque}';");
+            con.ExecutarSQL(sql);  
+        }
+
+        public DataTable ConsultarID(int Id) {
+            string sql = string.Format($@"SELECT * FROM produto WHERE id= {Id};");
+            return con.ExecutarConsulta(sql);
+        }
+        public DataTable Pesquisar(string condicao) {
+
+            string sql = string.Format($@"SELECT p.id, p.nome, p.descricao, p.valor, 
+                                                 f.nome, c.nome, p.foto, p.quantidadeEstoque  
+                                        FROM produto as p, categoria as c, fornecedor as f 
+                                        WHERE p.categoriaID = c.id and p.fornecedorID = f.id 
+                                        and {condicao} ORDER BY p.id;");
+            return con.ExecutarConsulta(sql);
+        }
        
     }
 }
