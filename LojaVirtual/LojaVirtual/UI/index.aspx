247669fa3e0bf35942bd2a46d837b111c3986b36
profRafaCotemig <rafaelapriscila@cotemig.com>
2021-04-14 08:13:41 -0300

    Update Index

--- LojaVirtual/LojaVirtual/UI/index.aspx
+++ LojaVirtual/LojaVirtual/UI/index.aspx
@@ -12,18 +12,32 @@
 <body>
     <form id="form1" runat="server">
         <nav>
-
         <div>
                 <ul>
                     <li>
                         <asp:HyperLink ID="HyperLinkInicial" NavigateUrl="~/UI/index.aspx" Text="Inicial" runat="server"></asp:HyperLink>
                     </li>
-
-
+                    <li>
+                    <asp:HyperLink ID="HyperLinkProdutos" NavigateUrl="~/UI/formTableProdutos.aspx" Text="Produtos" runat="server"></asp:HyperLink>
+                    </li>
+                     <li>
+                    <asp:HyperLink ID="HyperLinkFornecedores" NavigateUrl="~/UI/formTableFornecedores.aspx" Text="Fornecedores" runat="server"></asp:HyperLink>
+                    </li>
+                    <li>
+                    <asp:HyperLink ID="HyperLinkLogin" NavigateUrl="~/UI/formLogin.aspx" Text="Login" runat="server"></asp:HyperLink>
+                    </li>
                 </ul>
         </div>
         </nav>
-            
+       <main>
+           <asp:Image ImageUrl="~/IMG/game.jpg" AlternateText="Imagem não carregada." runat="server"/>
+       </main>
+        
+        <footer>
+
+            Copyright &copy; Desenvolvido por Rafaela. Todos os direitos reservados.
+ 
+       </footer>
        
     </form>
 </body>
--- LojaVirtual/LojaVirtual/UI/index.aspx.designer.cs
+++ LojaVirtual/LojaVirtual/UI/index.aspx.designer.cs
@@ -31,5 +31,32 @@ namespace LojaVirtual.UI
         /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
         /// </remarks>
         protected global::System.Web.UI.WebControls.HyperLink HyperLinkInicial;
+
+        /// <summary>
+        /// Controle HyperLinkProdutos.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLinkProdutos;
+
+        /// <summary>
+        /// Controle HyperLinkFornecedores.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLinkFornecedores;
+
+        /// <summary>
+        /// Controle HyperLinkLogin.
+        /// </summary>
+        /// <remarks>
+        /// Campo gerado automaticamente.
+        /// Modificar a declaração do campo de movimento do arquivo de designer para o arquivo code-behind.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.HyperLink HyperLinkLogin;
     }
 }
